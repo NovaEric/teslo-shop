ecfa2d1f707f936e8ae66e533f5887fdaad34e23
Eric Nova <ericnova23@hotmail.com>
2024-07-12 01:03:41 -0500

    Seccion 16 | Semilla de base de datos - Productos

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,18 +1,47 @@
-import prisma from '@/lib/prisma';
+import prisma from '../lib/prisma';
 import { initialData } from './seed';
 async function main() {
-    
-await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-])
+
+    //await Promise.all([
+       await prisma.productImage.deleteMany()
+       await prisma.product.deleteMany()
+       await prisma.category.deleteMany()
+    //])
+
+    const { products, categories } = initialData;
 
 
-    console.log("Seed ran succesfully!");
+    const categoriesData = categories.map((name) => ({ name }))
+
+    await prisma.category.createMany({
+        data: categoriesData
+    });
+
+    const categoriesDB = await prisma.category.findMany();
+
+    const categoriesMap = categoriesDB.reduce((map, category) => {
+
+        map[category.name.toLowerCase()] = category.id;
+        return map;
+    }, {} as Record<string, string>);
+
+
+
+
+    products.forEach(async(product) => {
+        const { type, images, ...rest } = product;
+
+        const dbProduct = await prisma.product.create({
+            data: {
+                ...rest,
+                categoryId: categoriesMap[type]
+            }
+        })
+    })
+
 }
 
-(() => { 
+(() => {
     if (process.env.NODE_ENV === 'production') return;
-    main(); 
+    main();
 })();
