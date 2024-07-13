5f22ff92b2460716609ca593d8fb310803258e8b
Eric Nova <ericnova23@hotmail.com>
2024-07-13 02:50:01 -0500

    Mostrar productos en pantalla

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,9 +1,13 @@
+import { getPaginatedProductsWithImages } from "@/actions";
 import { ProductGrid, Title } from "@/components";
 import { initialData } from "@/seed/seed";
 
-export default function Shop() {
-  const products = initialData.products;
 
+export default async function Shop() {
+
+  const { products } = await getPaginatedProductsWithImages();
+
+  console.log(products)
   return (
     <>
       <Title title="Tienda" subtitle="Todos los productos" className="mb-2" />
