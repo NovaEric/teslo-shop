298c1ad1682fd4899548c279831a47eeacef403c
Eric Nova <ericnova23@hotmail.com>
2024-09-08 23:35:16 -0500

    Mostrar errores en pantalla

--- src/app/auth/new-account/ui/RegisterForm.tsx
+++ src/app/auth/new-account/ui/RegisterForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import clsx from "clsx";
 import Link from "next/link";
 import { SubmitHandler, useForm } from "react-hook-form";
 
@@ -17,21 +18,29 @@ export const RegisterForm = () => {
 
         const { name, email, password } = data;
         console.log(data)
+        //Server action
     }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
 
+            <label htmlFor="email">Full Name</label>
             {
                 errors.name?.type === 'required' && (
-                    <span className="text-red-500">Name is required!</span>
+                    <span className="text-red-500">Name is required</span>
                 )
 
             }
-            <label htmlFor="email">Full Name</label>
             <input
                 placeholder="Full Name"
-                className="px-5 py-2 border bg-gray-200 rounded mb-5"
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': errors.name
+                        }
+                    )
+                }
                 type="text"
                 autoFocus
                 {...register('name', { required: true })}
@@ -39,22 +48,47 @@ export const RegisterForm = () => {
 
 
             <label htmlFor="email">Email</label>
+            {
+                errors.email?.type === 'required' && (
+                    <span className="text-red-500">Must be a valid email</span>
+                )
+
+            }
             <input
                 placeholder="Email"
-                className="px-5 py-2 border bg-gray-200 rounded mb-5"
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': errors.email
+                        }
+                    )
+                }
                 type="email"
                 {...register('email', { required: true, pattern: /^\S+@\S+$/i })}
             />
 
 
             <label htmlFor="password">Password</label>
+            {
+                errors.password?.type === 'required' && (
+                    <span className="text-red-500">Password Must be 6 characters minimum </span>
+                )
+
+            }
             <input
                 placeholder="Password"
-                className="px-5 py-2 border bg-gray-200 rounded mb-5"
+                className={
+                    clsx(
+                        "px-5 py-2 border bg-gray-200 rounded mb-5",
+                        {
+                            'border-red-500': errors.password
+                        }
+                    )
+                }
                 type="password"
-                {...register('password', { required: true })}
+                {...register('password', { required: true, minLength: 6 })}
             />
-
             <button
                 className="btn-primary">
                 Create Account
