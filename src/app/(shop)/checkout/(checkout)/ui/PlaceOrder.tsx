10d802d8f02d67b2a3fd4f3f30586f8ef47209c6
NovaEric <ericnova23@hotmail.com>
2024-09-23 00:22:51 -0500

    S22 - Bloquear botón y mensajes de error

--- src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
+++ src/app/(shop)/checkout/(checkout)/ui/PlaceOrder.tsx
@@ -1,78 +1,103 @@
-'use client'
-import { useAddressStore, useCartStore } from '@/store';
-import { currencyFormat } from '@/utils';
-import React, { useEffect, useState } from 'react'
+"use client";
+import { useAddressStore, useCartStore } from "@/store";
+import { currencyFormat } from "@/utils";
+import clsx from "clsx";
+import React, { useEffect, useState } from "react";
 
 export const PlaceOrder = () => {
+  const [loaded, setLoaded] = useState(true);
+  const [isPlacingOrder, setIsPlacingOrder] = useState(false);
+  const address = useAddressStore((state) => state.address);
+  const { subTotal, getTotalItems, tax, total } = useCartStore((state) =>
+    state.getSummaryInformation()
+  );
+  const cart = useCartStore(state => state.cart);
 
-    const [loaded, setLoaded] = useState(true);
-    const address = useAddressStore(state => state.address);
-    const { subTotal, getTotalItems, tax, total } = useCartStore(state => state.getSummaryInformation());
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
 
-    useEffect(()=>{
-        setLoaded(true);
-    }, []);
+  const onPlaceOrder = async () => {
+    setIsPlacingOrder(true);
 
-    if (!loaded) {
-        return <p>Loading...</p>
-    };
+    const productsToOrder = cart.map( product => ({
+        productId: product.id,
+        quantity: product.quantity,
+        size: product.sizes
+    }))
+    console.log({ address, productsToOrder })
+    setIsPlacingOrder(false);
+  };
+
+  if (!loaded) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div className="bg-white rounded-xl shadow-xl p-7">
-            <h2 className="text-2xl mb-2">Shipping address</h2>
-            <div className="mb-10">
-              <p className="text-xl">{address.firstName} {address.lastName}</p>
-              <p>{address.address}</p>
-              <p>{address.address2}</p>
-              <p>{address.postalCode}</p>
-              <p>{address.city}, {address.country}</p>
-              <p>{address.phone}</p>
-              
-            </div>
+      <h2 className="text-2xl mb-2">Shipping address</h2>
+      <div className="mb-10">
+        <p className="text-xl">
+          {address.firstName} {address.lastName}
+        </p>
+        <p>{address.address}</p>
+        <p>{address.address2}</p>
+        <p>{address.postalCode}</p>
+        <p>
+          {address.city}, {address.country}
+        </p>
+        <p>{address.phone}</p>
+      </div>
 
-            {/* Divider */}
-            <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
+      {/* Divider */}
+      <div className="w-full h-0.5 rounded bg-gray-200 mb-10" />
 
-            <h2 className="text-2xl mb-2">Order Summary</h2>
+      <h2 className="text-2xl mb-2">Order Summary</h2>
 
-            <div className="grid grid-cols-2">
-            <div className="grid grid-cols-2">
-            <span>Products Qty.</span>
-            <span className="text-right">{ getTotalItems() === 1 ? '1 Item' :  getTotalItems() + ' Items'} </span>
+      <div className="grid grid-cols-2">
+        <div className="grid grid-cols-2">
+          <span>Products Qty.</span>
+          <span className="text-right">
+            {getTotalItems() === 1 ? "1 Item" : getTotalItems() + " Items"}{" "}
+          </span>
 
-            <span>Subtotal</span>
-            <span className="text-right">${currencyFormat(subTotal)}</span>
+          <span>Subtotal</span>
+          <span className="text-right">${currencyFormat(subTotal)}</span>
 
-            <span>Tax (15%)</span>
-            <span className="text-right">${currencyFormat(tax)}</span>
+          <span>Tax (15%)</span>
+          <span className="text-right">${currencyFormat(tax)}</span>
 
-            <span className="mt-5 text-2xl">Total:</span>
-            <span className="mt-5 text-2xl text-right">${currencyFormat(total)}</span>
+          <span className="mt-5 text-2xl">Total:</span>
+          <span className="mt-5 text-2xl text-right">
+            ${currencyFormat(total)}
+          </span>
         </div>
-            </div>
+      </div>
 
-            <div className="mt-5 mb-2 w-full">
-              <p className="mb-5">
-                {/* Disclaimer */}
-                <span className="text-xs">
-                  By clicking you accept{" "}
-                  <a href="#" className="underline">
-                    terms and conditions
-                  </a>{" "}
-                  &{" "}
-                  <a href="#" className="underline">
-                    privacy policy
-                  </a>
-                </span>
-              </p>
+      <div className="mt-5 mb-2 w-full">
+        <p className="mb-5">
+          {/* Disclaimer */}
+          <span className="text-xs">
+            By clicking you accept{" "}
+            <a href="#" className="underline">
+              terms and conditions
+            </a>{" "}
+            &{" "}
+            <a href="#" className="underline">
+              privacy policy
+            </a>
+          </span>
+        </p>
 
-              <button
-                className="flex btn-primary justify-center"
-                
-              >
-                place order
-              </button>
-            </div>
-          </div>
-  )
-}
+        {/* <p className="text-red-500"> Error placing order </p> */}
+        <button 
+        onClick={onPlaceOrder}
+        className={clsx({
+            'btn-primary': !isPlacingOrder,
+            'btn-disabled': isPlacingOrder
+            
+        })}>place order</button>
+      </div>
+    </div>
+  );
+};
