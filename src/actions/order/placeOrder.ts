ce8f3b0caa5a06dc7a5fd3a0b292d9812cf96725
NovaEric <ericnova23@hotmail.com>
2024-09-30 23:45:23 -0500

    S22 - Colocar orden - Productos y cantidades

--- src/actions/order/placeOrder.ts
+++ src/actions/order/placeOrder.ts
@@ -2,6 +2,7 @@
 
 import { auth } from "@/auth.config";
 import { IAddress, Size } from "@/interfaces";
+import prisma from "@/lib/prisma";
 
 interface ProductToOrder {
     productId: string;
@@ -22,6 +23,18 @@ export const placeOrder = async(productIds: ProductToOrder[], address: IAddress 
         }
     }
 
-    console.log({ productIds, address, userId } )
+    // Get products information
+    // PD: We can not have 2 products with the same ID
+
+    const products = await prisma.product.findMany({
+        where:{
+            id: {
+                in: productIds.map(p => p.productId)
+            }
+        }
+    });
+
+    // Calculate amount
+    const itemsInOrder = productIds.reduce((count, p) => count + p.quantity , 0);
 
 }
