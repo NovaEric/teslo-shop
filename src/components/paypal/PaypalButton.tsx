38cf55f1ab516a2f0783352755a81f276be42dbc
NovaEric <ericnova23@hotmail.com>
2024-10-19 21:52:48 -0500

    S23 - Mostrar Skeleton mientras se carga

--- src/components/paypal/PaypalButton.tsx
+++ src/components/paypal/PaypalButton.tsx
@@ -1,9 +1,22 @@
 'use client'
 
-import { PayPalButtons } from '@paypal/react-paypal-js'
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js'
 
 
 export const PaypalButton = () => {
+
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className='animate-pulse mb-16'>
+        <div className='h-12 bg-gray-300 rounded'></div>
+        <div className='h-12 bg-gray-300 rounded mt-2'></div>
+        <div className='h-12 bg-gray-300 rounded mt-2'></div>
+      </div>
+    )
+  }
+
   return (
     <PayPalButtons/>
   )
