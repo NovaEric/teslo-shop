01d78533bff5ba97ec92a4fb528c0752b05936e0
NovaEric <ericnova23@hotmail.com>
2024-10-27 01:59:29 -0500

    S23 - Server Action - Verificar Pago - Parte 2

--- src/actions/payments/paypalCheckPayment.ts
+++ src/actions/payments/paypalCheckPayment.ts
@@ -1,5 +1,48 @@
-'use server'
+"use server";
 
-export const paypalCheckPayment = async(paypalTransactionId: string) => {
-    console.log(paypalTransactionId)
-} 
+export const paypalCheckPayment = async (paypalTransactionId: string) => {
+  const authToken = await getPaypalBearerToken();
+  console.log({authToken});
+
+  if (!authToken) {
+    return {
+        ok: false,
+        message: 'Could not get token'
+    }
+  }
+};
+
+const getPaypalBearerToken = async (): Promise<string|null> => {
+  const base64Token = Buffer.from(
+    `${process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID}:${process.env.PAYPAL_SECRET}`,
+    "utf-8"
+  ).toString("base64");
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  myHeaders.append("Authorization", `Basic ${base64Token}`);
+
+  const urlencoded = new URLSearchParams();
+  urlencoded.append("grant_type", "client_credentials");
+  urlencoded.append("ignoreCache", "true");
+  urlencoded.append("return_authn_schemes", "true");
+  urlencoded.append("return_client_metadata", "true");
+  urlencoded.append("return_unconsented_scopes", "true");
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    body: urlencoded,
+  };
+
+  try {
+    const result = await fetch(
+      process.env.PAYPAL_OAUTH_URL ?? "",
+      requestOptions
+    ).then((response) => response.json());
+
+    return result.access_token;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
